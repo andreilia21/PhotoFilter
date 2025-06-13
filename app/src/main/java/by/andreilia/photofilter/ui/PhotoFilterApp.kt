3bc814aa74388e402ae8f0e37cea35f3c9affa17
r0astPiGGy <1forzyg1@gmail.com>
2025-06-13 13:59:51 +0300

    Add photo picker

--- app/src/main/java/by/andreilia/photofilter/ui/PhotoFilterApp.kt
+++ app/src/main/java/by/andreilia/photofilter/ui/PhotoFilterApp.kt
@@ -1,6 +1,21 @@
 package by.andreilia.photofilter.ui
 
+import android.R.attr.bitmap
+import android.R.attr.contentDescription
+import android.content.ContentResolver
+import android.content.Context
+import android.content.Intent
 import android.content.res.Configuration
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.ImageDecoder
+import android.net.Uri
+import android.os.Build
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -31,13 +46,25 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.graphics.painter.BitmapPainter
+import androidx.compose.ui.input.key.Key.Companion.I
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import by.andreilia.photofilter.ui.theme.AppTheme
+import kotlinx.coroutines.launch
 
 @Preview(showSystemUi = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
 @Composable
@@ -47,8 +74,37 @@ fun ScreenPreview() {
     }
 }
 
+fun Context.loadBitmap(uri: Uri): Bitmap? {
+    // Check the API level to use the appropriate method for decoding the Bitmap
+    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+        // For Android P (API level 28) and higher, use ImageDecoder to decode the Bitmap
+        val source = ImageDecoder.createSource(contentResolver, uri)
+        ImageDecoder.decodeBitmap(source)
+    } else {
+        // For versions prior to Android P, use BitmapFactory to decode the Bitmap
+        val bitmap = contentResolver.openInputStream(uri)?.use { stream ->
+            Bitmap.createBitmap(BitmapFactory.decodeStream(stream))
+        }
+        bitmap
+    }
+}
+
 @Composable
 fun PhotoFilterApp() {
+    var selectedPhoto by remember { mutableStateOf<ImageBitmap?>(null) }
+
+    val context = LocalContext.current
+
+    val photoPicker = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.PickVisualMedia()
+    ) { uri ->
+        uri?.let {
+            val flag = Intent.FLAG_GRANT_READ_URI_PERMISSION
+            context.contentResolver.takePersistableUriPermission(uri, flag)
+            selectedPhoto = context.loadBitmap(uri)?.asImageBitmap()
+        }
+    }
+
     Scaffold(
         contentWindowInsets = WindowInsets.statusBars
     ) { paddingValues ->
@@ -57,15 +113,27 @@ fun PhotoFilterApp() {
                 .padding(paddingValues)
                 .fillMaxSize(),
         ) {
-            Box(
-                modifier = Modifier
-                    .padding(horizontal = 16.dp)
-                    .weight(1f)
-                    .fillMaxWidth()
-                    .clip(RoundedCornerShape(16.dp))
-                    .background(MaterialTheme.colorScheme.surfaceContainer),
-            ) {
-
+            val photo = selectedPhoto
+            if (photo != null) {
+                Image(
+                    modifier = Modifier
+                        .padding(horizontal = 16.dp)
+                        .weight(1f)
+                        .fillMaxWidth()
+                        .clip(RoundedCornerShape(16.dp))
+                        .background(MaterialTheme.colorScheme.surfaceContainer),
+                    bitmap = photo,
+                    contentDescription = null,
+                )
+            } else {
+                Box(
+                    modifier = Modifier
+                        .padding(horizontal = 16.dp)
+                        .weight(1f)
+                        .fillMaxWidth()
+                        .clip(RoundedCornerShape(16.dp))
+                        .background(MaterialTheme.colorScheme.surfaceContainer),
+                )
             }
             Row(
                 modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp)
@@ -107,7 +175,7 @@ fun PhotoFilterApp() {
                     IconButton(
                         modifier = Modifier.align(alignment = Alignment.CenterStart),
                         onClick = {
-
+                            photoPicker.launch(PickVisualMediaRequest(mediaType = ActivityResultContracts.PickVisualMedia.ImageOnly))
                         }
                     ) {
                         Icon(Icons.Outlined.AddBox, contentDescription = null)
